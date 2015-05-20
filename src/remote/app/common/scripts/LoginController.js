e569fa1658a8a1c9b3c4875c97c3119b970099e4
friendly-flame <armsnyder@gmail.com>
2015-05-20 04:52:55 -0500

    New gitignore

--- src/remote/app/common/scripts/LoginController.js
+++ src/remote/app/common/scripts/LoginController.js
@@ -1,9 +1,13 @@
 angular
     .module('common')
-    .controller('LoginController', function($scope, supersonic) {
+    .controller('LoginController', function($scope, supersonic, $http) {
+        var serverPort = '8001';
+        $scope.loginEnabled = true;
         $scope.playerName = '';
         $scope.serverIP = '';
         $scope.submit = function() {
-            supersonic.ui.initialView.dismiss()
+            $scope.loginEnabled = false;
+            $http.post('http://'+$scope.serverIP+':'+serverPort+'/ping', {playerName: $scope.playerName});
+            supersonic.ui.initialView.dismiss();
         }
     });
