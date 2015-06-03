a85bcd5b67d98babafe9916e41d6b969aff60014
friendly-flame <armsnyder@gmail.com>
2015-06-03 01:16:16 -0500

    start_server started

--- src/hub/start_server.py
+++ src/hub/start_server.py
@@ -10,93 +10,114 @@ import Queue
 import urlparse
 import time
 import screens
+import os
 
 
 httpd = None
 control_queue = Queue.Queue()
 players = {}
-game = None
 serverIP = ''
 move_on = False
+screen = None
 
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def __init__(self, request, client_address, server):
         BaseHTTPServer.BaseHTTPRequestHandler.__init__(self, request, client_address, server)
+    #
+    # def do_GET(self):
+    #     parsed_path = urlparse.urlparse(self.path)
+    #     message_parts = []
+    #     for name, value in sorted(self.headers.items()):
+    #         message_parts.append('%s=%s' % (name, value.rstrip()))
+    #     message_parts.append('')
+    #     message = '\r\n'.join(message_parts)
+    #     self.send_response(200)
+    #     self.end_headers()
+    #     self.wfile.write(message)
+    #     return
 
-    def do_POST(self):
+    def do_GET(self):
+        global screen
         parsed_path = urlparse.urlparse(self.path)
-        ctype, pdict = cgi.parse_header(self.headers['content-type'])
-        if ctype == 'application/json':
-            content_len = int(self.headers.getheader('content-length', 0))
-            post_body = json.loads(self.rfile.read(content_len))
+        try:
+            post_body = json.loads('{'+parsed_path.query+'}')
+        except Exception:
+            post_body = parsed_path.query
+
+        if parsed_path.path == '/button':
             self.update_connected_players(post_body)
+            post_body['playerId'] = self.address_string()
+            control_queue.put(post_body)
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
 
-            if parsed_path.path == '/button':
-                post_body['playerId'] = self.address_string()
-                control_queue.put(post_body)
-                self.send_response(200)
-                self.send_header('Access-Control-Allow-Origin', '*')
-                self.end_headers()
+        elif parsed_path.path == '/ask_ready':
+            self.update_connected_players(post_body)
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({'ready': screen == 'game'}))
 
-            elif parsed_path.path == '/ask_ready':
-                self.send_response(200)
-                self.send_header('Access-Control-Allow-Origin', '*')
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                if game.screen.id == 'game':
-                    self.wfile.write('{"ready": true}')
-                else:
-                    self.wfile.write('{"ready": false}')
+        elif parsed_path.path == '/gamePing':
+            self.update_connected_players(post_body)
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response = {
+                'ready': True,
+                'players': [{
+                    'name': players[player]['player'].player_name,
+                    'id': player
+                } for player in players.keys() if player != self.address_string()]
+            }
+            print response
+            self.wfile.write(json.dumps(response))
 
-            elif parsed_path.path == '/gamePing':
-                print(game.screen.id)
-                self.send_response(200)
-                self.send_header('Access-Control-Allow-Origin', '*')
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                if game.screen.id == 'gameover':
-                    response = {
-                        'ready': True,
-                        'players': [{
-                            'name': players[player]['player'].player_name,
-                            'id': player
-                        } for player in players.keys() if player != self.address_string()]
-                    }
-                    print response
-                    self.wfile.write(json.dumps(response))
-                else:
-                    print 'not ready'
-                    self.wfile.write('{"ready": false}')
-                    
-            elif parsed_path.path == '/vote':
-                self.send_response(200)
-                self.send_header('Access-Control-Allow-Origin', '*')
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                players[self.address_string()]['vote'] = post_body['vote']
-                voted_players = []
-                ready = True
-                print players
-                for player in players.values():
-                    if player['vote'] is None:
-                        ready = False
-                        print player['vote'], 'not ready'
-                        break
-                    voted_players.extend(player['vote'])
-                if ready:
-                    print 'ready!'
-                    voted_players = set(voted_players)
-                    for player in voted_players:
-                        players[player]['exiled'] = True
-                        print 'loading screen'
-                    game.load_screen(screens.GameScreen)
+        elif parsed_path.path == '/vote':
+            self.update_connected_players(post_body)
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            players[self.address_string()]['vote'] = post_body['vote']
 
-        else:
-            print ctype
-            print pdict
+        elif parsed_path.path == '/tally':
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            voted_players = []
+            ready = True
+            for player in players.values():
+                if player['vote'] is None:
+                    ready = False
+                    print player['vote'], 'not ready'
+                    break
+                voted_players.extend(player['vote'])
+            if ready:
+                print 'ready!'
+                voted_players = set(voted_players)
+                for player in voted_players:
+                    players[player]['exiled'] = True
+                    print 'loading screen'
+                self.wfile.write(json.dumps(True))
+            else:
+                self.wfile.write(json.dumps(False))
+
+        elif parsed_path.path == '/set_screen':
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            screen = post_body
+            print screen
         self.connection.shutdown(1)
+
 
     def do_OPTIONS(self):
         self.send_response(200, 'ok')
@@ -110,7 +131,7 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         player_id = self.address_string()
         if player_id in players:
             players[player_id]['last_ping'] = time.time()
-        elif game.screen.id == 'lobby':
+        elif screen == 'lobby':
             if 'playerName' in post_body:
                 players[player_id] = {
                     'last_ping': time.time(),
@@ -138,9 +159,8 @@ class ThreadedHTTPServer(SocketServer.ThreadingMixIn, MyServer):
     """Handle requests in a separate thread."""
 
 
-def start_server(game_arg):
-    global httpd, game, serverIP
-    game = game_arg
+def start_server():
+    global httpd, serverIP
     if httpd:
         print 'Server is already running'
         return
@@ -167,3 +187,7 @@ def stop_server():
 
 def get_address():
     return socket.gethostbyname(socket.gethostname())
+
+
+if __name__ == '__main__':
+    start_server()
