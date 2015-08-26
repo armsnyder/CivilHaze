0a8e6f45982a195db7500c43e2d0c4b988ca06f0
friendly-flame <armsnyder@gmail.com>
2015-08-26 14:11:18 -0400

    Removed old folders properly

--- src/hub/util.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Adam Snyder'
-
-import random
-
-
-def random_color():
-    vals = '3456789ABCDE'
-    return '#%s%s%s' % (random.choice(vals), random.choice(vals), random.choice(vals))
