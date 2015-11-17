7266a1dacb74c29df380e5c4dd2b5060fb885a92
friendly-flame <armsnyder@gmail.com>
2015-11-17 12:08:03 -0600

    starting database

---
+++ web/public/javascripts/ControllerService.js
@@ -0,0 +1,8 @@
+angular.module('come-again')
+    .factory('ControllerService', function($http) {
+        var factory = {};
+        factory.getGameIP = function() {
+
+        };
+        return factory;
+    });
