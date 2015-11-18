3a7a56099266ae0e7e33d4144835ec806dc43e71
friendly-flame <armsnyder@gmail.com>
2015-11-17 20:43:42 -0600

    secret json

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var MobileDetect = require('mobile-detect');
+var fs = require('fs'), json;
+var secret = JSON.parse(getConfig('../secret.json'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,10 +10,26 @@ router.get('/', function(req, res, next) {
     if (md.mobile()) {
         res.render('mobileIndex', { title: 'Controller' });
     } else {
-        res.render('desktopIndex', { title: 'Come Again' });
+        res.render('desktopIndex', { title: secret.database.foo });
     }
 });
 
 router.get('/api/gameID');
 
 module.exports = router;
+
+// Utility functions:
+function readJsonFileSync(filepath, encoding) {
+
+    if (typeof (encoding) == 'undefined'){
+        encoding = 'utf8';
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
+
+function getConfig(file) {
+
+    var filepath = __dirname + '/' + file;
+    return readJsonFileSync(filepath);
+}
