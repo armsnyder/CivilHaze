32e0e78c1258f777d8479aa1e7b2b37d28d9b5b6
friendly-flame <armsnyder@gmail.com>
2015-11-19 00:19:53 -0600

    post IP api

--- web/routes/api.js
+++ web/routes/api.js
@@ -10,6 +10,7 @@ var connectionPool = mysql.createPool(secret.mysqlConnection);
 // GET
 
 exports.getPrivate = function(req, res) {
+    //TODO: For added security, check IP of client
     var publicIP = req.params.publicIP;
     connectionPool.getConnection(function(err, connection) {
         if (err) {
@@ -17,39 +18,37 @@ exports.getPrivate = function(req, res) {
             res.statusCode = 503;
             res.json({
                 result: 'error',
-                err: err.code
+                error: err.code
             });
         } else {
             connection.query('SELECT * FROM games WHERE public_ip='+publicIP+" ORDER BY last_updated DESC",
                 function(err, rows) {
-                if (err) {
-                    console.error(err);
-                    res.statusCode = 500;
-                    res.json({
-                        result: 'error',
-                        err: err.code
-                    })
-                } else {
-                    if (rows.length == 0) {
-                        err = 'Could not find requested IP';
+                    if (err) {
                         console.error(err);
-                        res.statusCode = 404;
+                        res.statusCode = 500;
                         res.json({
                             result: 'error',
-                            err: err
+                            error: err.code
                         })
                     } else {
-                        //TODO: Account for multiple valid games on a single subnet
-                        res.json({
-                            result: rows[0],
-                            err: '',
-                            ip: req.ip,
-                            ipNum: ip.toLong(req.ip)
-                        })
+                        if (rows.length == 0) {
+                            err = 'Could not find requested IP';
+                            console.error(err);
+                            res.statusCode = 404;
+                            res.json({
+                                result: 'error',
+                                error: err
+                            })
+                        } else {
+                            //TODO: Account for multiple valid games on a single subnet
+                            res.json({
+                                result: rows[0],
+                                error: ''
+                            })
+                        }
                     }
-                }
-                connection.release();
-            });
+                    connection.release();
+                });
         }
     });
 };
@@ -57,7 +56,44 @@ exports.getPrivate = function(req, res) {
 // POST
 
 exports.postPrivate = function(req, res) {
-
+    //TODO: For added security, ensure client IP matches publicIP
+    if ('publicIP' in req.body && 'privateIP' in req.body) {
+        connectionPool.getConnection(function(err, connection) {
+            if (err) {
+                console.error('CONNECTION error: ', err);
+                res.statusCode = 503;
+                res.json({
+                    result: 'error',
+                    error: err.code
+                });
+            } else {
+                connection.query('INSERT INTO games (public_ip, private_ip) VALUES ('+
+                    req.body.publicIP+', '+req.body.privateIP+')',
+                    function(err) {
+                        if (err) {
+                            console.error(err);
+                            res.statusCode = 500;
+                            res.json({
+                                result: 'error',
+                                error: err.code
+                            })
+                        } else {
+                            res.json({
+                                result: 'success',
+                                error: ''
+                            })
+                        }
+                        connection.release();
+                    });
+            }
+        });
+    } else {
+        res.statusCode = 404;
+        res.json({
+            result: 'error',
+            error: 'Invalid request'
+        })
+    }
 };
 
 // Utility functions
