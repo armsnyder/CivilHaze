32a5239e1f08bfd50077c6c0aff516599d3fd4f1
friendly-flame <armsnyder@gmail.com>
2015-11-20 04:49:05 -0600

    began test framework and GameRouteService

--- web/public/js/services.js
+++ web/public/js/services.js
@@ -5,4 +5,16 @@ angular.module('comeAgain')
 
         };
         return factory;
+    })
+    .factory('GameRouteService', function($http) {
+        var MASTER_URL = "http://come-again.net";
+        var MY_IP_URL = "http://checkip.amazonaws.com";  //TODO: Don't rely on external source
+        return {
+            getGameIP: function() {
+                //TODO: Make sure error on first call is reflected in promise
+                return $http.get(MY_IP_URL).then(function(result) {
+                    return $http.get(MASTER_URL+"/api/private/"+result);
+                });
+            }
+        };
     });
