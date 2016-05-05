264175d7bcabb9be7e30e34b06ed150b80777521
Adam Snyder <armsnyder@gmail.com>
2016-05-05 12:29:03 -0500

    desktop connections redirected to project page

--- web/app.js
+++ web/app.js
@@ -13,6 +13,7 @@ var nib = require('nib');
 var Game = require('./Game.js');
 var ip = require('ip');
 var mongoose = require('mongoose');
+var device = require('device');
 
 var app = express();
 
@@ -158,7 +159,8 @@ app.get('/partials/:name', function(req, res) {
 // Redirect all others to main app
 
 app.get('*', function(req, res) {
-    res.render('index');
+    if (device(req.get('User-Agent')).is('desktop')) res.redirect('http://armsnyder.com/civil-haze');
+    else res.render('index');
 });
 
 // catch 404 and forward to error handler
